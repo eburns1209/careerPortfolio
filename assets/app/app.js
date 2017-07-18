ae127a529475b4b3017322446dc5be26f17126a9
eburns <eburns1209@gmail.com>
2017-07-17 21:29:38 -0400

    updated

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,38 +1,9 @@
-
-$("#submit").on("click", function (event){
-		event.preventDefault();
-
-	var name = $("#name-input").val().trim();
-	var email = $("#email-input").val().trim();
-	var message = $("#message-input").val().trim();
-
-
-	var newEmail = {
-		person: name,
-		contact: email,
-		story: message
-	};
+$(document).ready(function(){
 
 	
 
-	console.log(newEmail.person);
-	console.log(newEmail.contact);
-	console.log(newEmail.story);
 
-	alert("email sent");
 
-	$("#name-input").val("");
-	$("#email-input").val("");
-	$("#message-input").val("");
 
-	return false;
 });
 
-
-
-
-$("#btn").click(function(){
-	console.log("it worked");
-	// window.location.href = '../images/ErinStowe2017.pdf'
-	// console.log("it worked");
-})
