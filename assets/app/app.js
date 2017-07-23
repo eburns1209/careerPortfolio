b323cb17e123dce144adacafcd1eb87f571255b2
eburns <eburns1209@gmail.com>
2017-07-23 19:28:02 -0400

    updated

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,9 +1,32 @@
 $(document).ready(function(){
 
+$(".hire").click(function(){
+	$('#sub-footer').animate({
+		scrollTop: $(".page-scroll").offset().top},
+		'slow');
+	});
+
+
+$(".learn").click(function(){
+	$('#me, #myLife').animate({
+		scrollTop: $(".page-scroll").offset().top},
+		'slow');
+	});
+
+
+$(".touch").click(function(){
+	$('#sub-footer').animate({
+		scrollTop: $(".page-scroll").offset().top},
+		'slow');
+	});
+})
+
+
+
 	
 
 
 
 
-});
+
 
